baa8cf392b385fb9bccdbfe7fd2761f416ddf162
Subham Kar <karsubham669@gmail.com>
2026-06-18 11:37:57 +0530

    Add files via upload
    Quick Quiz: Write a program to print first ‘n’ natural number using do-while loop.

---
+++ quize2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    int i = 1;
+    scanf("%d", &n);
+    do
+    {
+        printf(" %d\n", i);
+        i++;
+    } while (i <= n);
+
+    return 0;
+}
